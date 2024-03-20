7077203beb4108e70108be3e6df841afd791d058
Abhinav Roy <72131607+AbhinavRoy99@users.noreply.github.com>
2024-03-20 20:41:45 +0530

    Add files via upload

--- Assignment 2/lib_ar.py
+++ Assignment 2/lib_ar.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 import scipy
 from scipy import sparse
+import lib_ar as toolsar
 
 
 #Fixed Point Method
@@ -42,7 +43,7 @@ def simpsons_rule(function, lower_limit, upper_limit, num_intervals):
 # Gaussian quadrature
 def gaussian_quadrature(f, a, b, n):
 	# Compute the sample points and weights from legendre polynomials
-	x, w = np.polynomial.legendre.leggauss(n)
+	x, w = np.polynomiaar.legendre.leggauss(n)
 	# Change of variables
 	t = 0.5 * (x + 1) * (b - a) + a
 	return np.sum(w * f(t)) * 0.5 * (b - a)
@@ -241,11 +242,11 @@ def backwardsub(l,k):
 
 # Function to calculate the RK4 step
 def rk4_step(f, x, y, h):
-    k1 = h * f(x, y)
-    k2 = h * f(x + 0.5*h, y + 0.5*k1)
-    k3 = h * f(x + 0.5*h, y + 0.5*k2)
-    k4 = h * f(x + h, y + k3)
-    return y + (k1 + 2*k2 + 2*k3 + k4) / 6
+	k1 = h * f(x, y)
+	k2 = h * f(x + 0.5*h, y + 0.5*k1)
+	k3 = h * f(x + 0.5*h, y + 0.5*k2)
+	k4 = h * f(x + h, y + k3)
+	return y + (k1 + 2*k2 + 2*k3 + k4) / 6
 
 
 #Regula Falsi Method
@@ -293,3 +294,451 @@ def newton_raphson(f, df, x, eps):
 		root_estimates.append(x)
 		f_estimate.append(f(x))
 	return x, root_estimates, f_estimate
+
+
+def gaussjordan(l,k):
+	x=len(l)
+	y=len(l[0])
+	
+	
+	for ok in range(0,x):
+		
+
+		
+		for i in range(ok+1, x):
+			if l[i][ok]>l[ok][ok]:
+				for j in range(ok,x):
+					l[ok][j],l[i][j]=l[i][j],l[ok][j]
+				k[ok],k[i]=k[i],k[ok]
+		
+		t = l[ok][ok]
+		
+		if t != 0:
+			k[ok][0] = k[ok][0]/t
+			for a in range(ok,y):
+				l[ok][a] = l[ok][a]/t
+				
+		for a in [ x for x in range(0,x) if x!=ok ]:
+			lok=l[a][ok]
+			k[a][0] = k[a][0] - (lok*k[ok][0])
+			for b in range(0,y):
+				l[a][b] = l[a][b] - (lok*l[ok][b])
+	
+	for a in range(0,x):
+		for b in range(0,y):
+			l[a][b] = round((l[a][b]),3)
+
+	for a in range(0,x):
+		k[a][0] = round((k[a][0]),3)
+		
+	print('\n System of linear equations after Gauss Jordan Elimination is: \n')
+	for i in range(0,len(l)):
+		print(l[i],'(','X',i+1,')',k[i])
+	
+	print('\n The answer matrix X is: \n',k)
+
+
+
+def printeq(l,k,x,key):
+	print('\n',key,'system of linear equations is: \n')
+	for i in range(0,len(l)):
+		print(l[i],'(',x,i+1,')',k[i])
+
+def matrixtranspose(l):
+	m,n = len(l),len(l)
+	lt=[[0] * n for i in range(m)]
+	
+	for i in range(0,len(l)):
+		for j in range(0,len(l)):
+			lt[i][j]=l[j][i]
+	
+	return lt
+
+def checksym(l):
+	def check(l):
+		lt=toolsar.matrixtranspose(l)
+		for i in range(0,len(l)):
+			for j in range(0,len(l)):
+				if lt[i][j] != l[j][i]:
+					return False
+		return True
+	if (check(l)):
+		print('\n Yes the given matrix is symmetric.')
+	else:
+		print('\n No the given matrix is not symmetric.')
+
+def CholeskyD(content):
+	
+	print('\n The matrix to be decomposed is: \n')
+	for row in content:
+		print(row)
+	
+	n=len(content)
+		
+	
+	for j in range(0,n):
+		for i in range(0,n):
+			
+			s=0
+			
+			if j == i:
+				
+				for k in range(0,j):
+					s = s + (content[i][k]**2)
+					
+				content[i][i] = math.sqrt(content[i][i] - s)
+				
+				
+				
+			s=0
+			
+			if j < i:
+				
+				for k in range(0,j):
+					s = s + (content[i][k] * content[j][k])
+				
+				if(content[j][j] != 0):
+					content[i][j] = int(content[i][j] - s) / content[j][j]
+					
+				
+		
+			
+			
+			if j > i:
+				
+				content[i][j] = 0.0
+			 
+				
+				
+	for a in range(0,n):
+		for b in range(0,n):
+			content[a][b] = round((content[a][b]),1)
+	
+	print('\n The decomposed matrix is: \n')
+	for row in content:
+		print(row)   
+
+	
+	contenttranspose = toolsar.matrixtranspose(content)
+	print('\n The decomposed matrix transpose is: \n')
+	for row in contenttranspose:
+		print(row)
+	return content,contenttranspose
+	
+def forwardsub(l,k):
+	k1 = []
+	i=0
+	for i in range(0,len(l)):
+		k1.append(k[i][0])
+		for j in range(0,i):
+			k1[i]= k1[i]-(l[i][j]*k1[j])
+		k1[i]= k1[i]/l[i][i]
+	
+	m, n = len(l), 1
+	k2 = [[0] * n for i in range(m)]
+	
+	for i in range(0,len(l)):
+		k2[i][0]=k1[i]
+	
+	return k2
+
+def backwardsub(l,k):
+	m, n = len(l), 1
+	k1 = [[0] * n for i in range(m)]
+	k2 = [[0] * n for i in range(m)]
+	
+	for i in range(0,len(l)):
+		k2[i][0]=k[i][0]
+	
+	for i in range(len(l)-1,-1,-1):
+		for j in range(i+1,len(l)):
+			k2[i][0]= k2[i][0]-(l[i][j]*k1[j][0])
+		
+		k1[i][0]= k2[i][0]/l[i][i]
+	
+	return k1
+
+
+def diagonaldom(l,k):
+	lnew=[[0] * len(l) for i in range(len(l))]
+	knew=[0] * len(l)
+	for i in range(len(l)):
+		for j in range(len(l)):
+			lnew[i][j]=l[i][j]
+			knew[i]=k[i]
+	swapc=0
+	for i in range(len(l)):
+		summ=0
+		for t in range(len(l)):
+			summ = summ + abs(lnew[i][t])
+		for j in range(len(l)):
+			if abs(lnew[i][j])> (summ-l[i][j]):
+				lnew[i],lnew[j] = lnew[j], lnew[i]
+				knew[i], knew[j] = knew[j], knew[i]
+	
+	toolsar.printeq(lnew,knew,'X','The diaonally dominant')
+	return lnew,knew
+
+
+
+def arrclose(x,y,tol):
+	count=0
+	if len(x)==len(y):
+		for i in range(0,len(x)):
+			if (abs(x[i]-y[i]))/abs(y[i]) < tol:
+				count =count+1
+			else:
+				return False
+	if count==len(x):
+		return True
+
+
+
+def JacobiM(a,b,it,tol):
+	print("\n Jacobi Calculations: \n")
+	
+	x=[1]*len(a)
+	newx=[1]*len(a)
+	
+	
+	for i in range(0,it):
+		t=0
+		for t in range(0,len(x)):
+			x[t] = newx[t]
+		
+		newx = toolsar.jac(a,b,newx)
+	
+		print(i+1,".",newx)
+	
+		z = toolsar.arrclose(newx,x,tol)
+		if z == True:
+			print('\n The answer is: \n',newx,"\n after",i+1,"iterations.")
+			break
+		else:
+			continue
+
+def jac(a,b,x):
+	for i in range(0, len(a)):
+		d=b[i]
+		for j in range(0, len(a)):  
+			if (j != i):
+				d = d - (a[i][j]*x[j])
+		
+		x[i] = d / a[i][i]
+		
+	return x
+
+def gsm(a,x,b):
+						
+
+	for j in range(0, len(a)):               
+		  
+		d=b[j]
+		
+		for i in range(0, len(a)):     
+			if(j != i):
+				d = d - (a[j][i]*x[i])
+		
+		x[j] = d / a[j][j]
+		
+	return x
+
+def GaussSeidel(a,x,b,it,tol):
+	print("\n Gauss Seidel Calculations: \n")
+	
+	y=[]
+	
+	for i in range(0,len(x)):
+		y.append(x[i])
+	
+	for i in range(0,it):
+		t=0
+		for t in range(0,len(x)):
+			y[t] = x[t]
+	
+		x = toolsar.gsm(a,x,b)
+	
+		print(i+1,".",x)
+	
+		z = toolsar.arrclose(x,y,tol)
+		if z == True:
+			print('\n The answer is: \n',x,"\n after",i+1,"iterations.")
+			break
+		else:
+			continue
+			
+
+
+
+def Bracket(a,b,func):
+	
+	while (func(a)*func(b)>=0):
+		if (abs(func(a)) < abs(func(b))):
+			a = a - 1.5*(b-a)
+			print(a,b)
+			
+			
+		if (abs(func(a)) > abs(func(b))):
+			b = b + 1.5*(b-a)
+			print(a,b)
+	
+	return (a,b)
+
+			
+
+def Bisect(a,b,e,d,func):
+	c=0
+	t=0
+	print(t,">>",a,b,c)
+	
+		
+	while abs(a-b)>e:
+		while abs(func(a))>d:
+			c=(a+b)/2
+			
+			
+			if abs(func(c))<=d:
+				print(t+1,">>",a,b,c)
+				print("the solution is",'%10.4E' %c,"in",t+1,"iterations.")
+				return c
+			else:  
+				if (func(a)*func(c))<0:
+					b=c
+					t=t+1
+					print(t,">>",a,b,c)
+					
+				else:
+					a=c
+					t=t+1
+					print(t,">>",a,b,c)
+
+
+def RegulaFalsi(a,b,e,d,fn):
+	c0=0
+	c1=1
+	t=0
+	print(t,">>",a,b,c0,c1)
+	
+	while abs(c1-c0)>e:
+		while abs(fn(a))>d:
+			c1 = b-((b-a)*fn(b))/(fn(b)-fn(a))
+			
+			if abs(fn(c1))<=d:
+				print(t+1,">>",a,b,c0,c1)
+				print("the solution is",c1,"in",t+1,"iterations.")
+				return c1
+			else:
+				if (fn(a)*fn(c1))<0:
+					b=c1
+					c0=c1
+					t=t+1
+					print(t,">>",a,b,c0,c1)
+					
+				else:
+					a=c1
+					c0=c1
+					t=t+1
+					print(t,">>",a,b,c0,c1)
+
+
+	
+	
+
+def conjugate_gradient(A, b, x0, tol=1e-6, max_iter=100):
+	x = np.array(x0, dtype=float)  # Initial guess
+	r = b - np.dot(A, x)  # Initial residual
+	p = r.copy()  # Initial search direction
+	r_r = np.inner(r, r)
+   
+	for _ in range(max_iter):
+		Ap = np.dot(A, p)
+		alpha = r_r / np.inner(p, Ap)
+		x += alpha * p
+		r -= alpha * Ap
+
+		r_r_next = np.inner(r, r)
+
+		if np.linalg.norm(r) < tol: 
+			break
+
+		p = r + (r_r_next / r_r) * p
+		r_r = r_r_next
+
+	return x
+
+
+def calculate_norm(r):
+    """
+    Calculates the Euclidean norm (L2 norm) of a vector r and returns the Euclidean norm of the vector r.
+    """
+    norm_squared = sum(element ** 2 for element in r)
+    norm = norm_squared ** 0.5
+    return norm 
+	
+
+def matrix_A_ij(x):
+    """
+    Calculate the matrix-vector product Ax for the given vector x.
+    Returns The result of the matrix-vector product Ax.
+    """
+    m = 0.2
+    N = len(x)
+    delta = 1.0 / N
+    result = np.zeros_like(x)
+    
+    for i in range(N):
+        result[i] += (delta + m) * x[i]
+        result[i] -= 2 * delta * x[i]
+        result[i] += delta * x[(i + 1) % N]  # Periodic boundary condition
+        result[i] += delta * x[(i - 1) % N]  # Periodic boundary condition
+        result[i] += m ** 2 * delta * x[i] 
+    #print(result)
+    return result
+
+def conjugate_fly(matrix_A_ij, b, x0, tol=10**(-6), max_iter=100):
+    """
+    Conjugate Gradient method for solving linear systems Ax = b.
+    Returns: The approximate solution vector x and the List of residue norms at each iteration step.
+    """
+    it = 0
+    x = x0.copy()  # Initial guess
+    r = b - matrix_A_ij(x)  # Initial residual
+    p = r.copy()  # Initial search direction
+    residue_norms = [np.linalg.norm(r)]  # List to store residue norms
+
+    for k in range(max_iter):
+        Ap = matrix_A_ij(p)
+        alpha = np.dot(r, r) / np.dot(p, Ap)
+        x += alpha * p
+        r -= alpha * Ap
+        
+        beta = np.dot(r, r) / np.dot(r - alpha * Ap, r - alpha * Ap)
+        p = r - alpha * Ap + beta * p
+
+        residue_norm = np.linalg.norm(r)
+        residue_norms.append(residue_norm)
+        it= it+1
+        if residue_norm < tol:
+            break
+
+    return x, residue_norms, it
+
+
+def conjugate_inv(matrix_A_ij, b, x0, tol=10**(-6), max_iter=100):
+    N = len(b)
+    inverse_columns = []
+    
+    for i in range(N):
+        # Create the right-hand side vector for solving Ax = e_i
+        ei = np.zeros(N)
+        ei[i] = 1
+        
+        # Solve the equation Ax = e_i using Conjugate Gradient method
+        x, _, _ = conjugate_fly(matrix_A_ij, ei, x0, tol, max_iter)
+        
+        # Append the solution (column of the inverse matrix) to the list
+        inverse_columns.append(x)
+    
+    # Stack the columns of the inverse matrix horizontally to form the complete inverse matrix
+    A_inv = np.column_stack(np.round(inverse_columns,4))
+    return A_inv
